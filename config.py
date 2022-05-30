7441db7d992851c7412e21bf2bc4edbcb0749c0e
akumaru <isakulovDev@gmail.com>
2022-05-30 14:25:28 +0500

    Changing configuration.

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ from telebot import TeleBot
 
 BOT_TOKEN = ''
 ADMIN_ID = ''
+BOT_ID = BOT_TOKEN.split(':')[0]
+PARSE_MODE = ''
 
-bot = TeleBot(BOT_TOKEN)
+bot = TeleBot(BOT_TOKEN,parse_mode=PARSE_MODE)
