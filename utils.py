9abe43eefac11f92e176291b92de44f73359b039
akumaru <isakulovDev@gmail.com>
2022-05-30 14:26:25 +0500

    New extra function.

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-from os import stat
-
-
 def is_msg_from_admin(chat_id, user_id, bot):
-    status = ['owner','administrator']
+    status = ['creator','administrator']
 
     member_status = bot.get_chat_member(chat_id,user_id)
-    
+
+    return member_status.status in status
+
+def is_bot_admin(chat_id, user_id, bot):
+    status = ['administrator']
+
+    member_status = bot.get_chat_member(chat_id, user_id)
+
     return member_status.status in status
