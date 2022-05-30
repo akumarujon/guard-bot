640774ced732c388b24aff19162dbd4d96b2254a
akumaru <isakulovDev@gmail.com>
2022-05-30 14:25:58 +0500

    Added new handler for banning member.

--- handlers.py
+++ handlers.py
@@ -1,6 +1,14 @@
-from config import bot
+import time
+from config import BOT_ID, BOT_TOKEN, bot
 from telebot.types import Message
-from utils import is_msg_from_admin
+from utils import is_msg_from_admin, is_bot_admin
+
+
+#Checking is bot admin?
+chat_types = ['supergroup','group']
+updates = bot.get_updates()
+if updates[-1].message.chat.type in chat_types and is_bot_admin(updates[-1].message.chat.id, BOT_ID, bot)==False:
+    bot.send_message(updates[-1].message.chat.id, 'Bot guruhda admin emas.')
 
 @bot.message_handler(commands=['start'])
 def start(msg: Message):
@@ -12,7 +20,10 @@ def help(msg: Message):
     bot.send_message(msg.chat.id, "Your guide link.")
 
 # Ban chat member
-@bot.message_handler(commands=['kick'])
+@bot.message_handler(commands=['ban'])
 def ban_member(msg: Message):
     if is_msg_from_admin(msg.chat.id, msg.from_user.id,bot):
         bot.ban_chat_member(msg.chat.id, msg.reply_to_message.from_user.id)
+        bot.send_message(msg.chat.id,f"[{msg.reply_to_message.from_user.first_name}](tg://user?id={msg.reply_to_message.from_user.id}) ban qilindi.")
+    else:
+        bot.send_message(msg.chat.id,"Faqat adminlar bu buyruqni ishlata olishadi.")
